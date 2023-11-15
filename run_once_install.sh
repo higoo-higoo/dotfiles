08156041f2c8ed32ec8f97988db62aab815c603a
21higuchi <higuchi-yuki@g.ecc.u-tokyo.ac.jp>
2023-11-15 23:43:08 +0900

    lazygit

--- run_once_install.sh
+++ run_once_install.sh
@@ -21,3 +21,14 @@ if [[ ! -e "$HOME/.fzf" ]]; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install --no-completion --no-key-bindings --no-update-rc
 fi
+
+# lasygit
+if [[ "$(uname)" == "Darwin" ]]; then
+	brew intall lazygit
+elif [[ ! -e "/usr/local/bin/lazygit" ]]; then
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	tar xf lazygit.tar.gz lazygit
+	sudo install lazygit /usr/local/bin
+	rm lazygit.tar.gz lazygit
+fi
