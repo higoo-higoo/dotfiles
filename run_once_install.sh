0bfecec2d4f1641bc97aeb73d5a868b2b883f01d
higoo-higoo <higyuki1101@gmail.com>
2023-12-14 22:16:23 +0900

    もろもろ追加

--- run_once_install.sh
+++ run_once_install.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
+set -xe
+
+# neovim
+if [[ "$(uname)" == "Darwin" ]]; then
+	brew install nodebrew
+	nodebrew setup
+	echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zshrc
+	source ~/.zshrc
+else
+	sudo apt update
+	sudo apt -y install nodejs npm
+	sudo npm install n -g
+	sudo n stable
+	sudo apt -y remove nodejs npm
+fi
+	
+
 
 # peco
 if [[ "$(uname)" == "Darwin" ]]; then
 	brew install peco
+else
+	wget https://github.com/peco/peco/releases/download/v0.5.10/peco_linux_amd64.tar.gz -O /tmp/peco.tar.gz
+	tar -xvf /tmp/peco.tar.gz -C /tmp/
+	mv /tmp/peco_linux_amd64/peco ~/.local/bin/
 fi
-if ! type peco > /dev/null 2>&1;  then
-    wget https://github.com/peco/peco/releases/download/v0.5.10/peco_linux_amd64.tar.gz -O /tmp/peco.tar.gz
-    tar -xvf /tmp/peco.tar.gz -C /tmp/
-    mv /tmp/peco_linux_amd64/peco ~/.local/bin/
+
+# direnv
+if [[ "$(uname)" == "Darwin" ]]; then
+	brew install direnv
+else
+	sudo apt -y install direnv
+fi
+
+# exa
+if [[ "$(uname)" == "Darwin" ]]; then
+	brew install exa
+else
+	sudo apt -y install exa
 fi
 
 
@@ -25,9 +55,15 @@ fi
 # lasygit
 if [[ "$(uname)" == "Darwin" ]]; then
 	brew install lazygit
-elif [[ ! -e "/usr/local/bin/lazygit" ]]; then
-	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+elif [ $(uname -m) == "x86_64" ] ; then
+	export LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	tar xf lazygit.tar.gz lazygit
+	sudo install lazygit /usr/local/bin
+	rm lazygit.tar.gz lazygit
+else
+	export LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_arm64.tar.gz"
 	tar xf lazygit.tar.gz lazygit
 	sudo install lazygit /usr/local/bin
 	rm lazygit.tar.gz lazygit
