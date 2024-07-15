55f62c7193d8692813b3d750b4403e341a24b649
Yuki Higuchi <yasai831ai@gmail.com>
2024-07-15 21:25:53 +0900

    add pyrealsense2 and package for cv2

--- scripts/python.sh
+++ scripts/python.sh
@@ -6,3 +6,7 @@ curl -sSL https://install.python-poetry.org | python3 -
 pip install --upgrade pip
 
 pip install pyserial
+
+pip install pyrealsense2
+
+sudo apt install libcanberra-gtk-module libcanberra-gtk3-module -y
