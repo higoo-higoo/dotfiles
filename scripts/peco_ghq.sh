c59b2ad1383bd769fbb347b33478d6c9a3917e3a
Ubuntu <azureuser@higuchi-sandbox.f5x0v1drghaujpcrkspwgzu3db.lx.internal.cloudapp.net>
2025-07-06 00:22:20 +0000

    neko

--- scripts/peco_ghq.sh
+++ scripts/peco_ghq.sh
@@ -9,9 +9,11 @@ fi
 if [ "$(uname)" = "Darwin" ]; then
 	brew install peco
 	brew install shellcheck
+  brew install ghq
 else
 	wget https://github.com/peco/peco/releases/download/v0.5.10/peco_linux_amd64.tar.gz -O /tmp/peco.tar.gz
 	tar -xvf /tmp/peco.tar.gz -C /tmp/
 	mkdir -p ~/.local/bin
-	mv /tmp/peco_linux_amd64/peco ~/.local/bin/
+	mv /tmp/peco_linux_amd64/peco ~/.local/bin/peco
+	go install github.com/x-motemen/ghq@latest
 fi
