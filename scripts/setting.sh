fb98e1cb31035fd03c4592d08266d0d883d75afd
higoo-higoo <higyuki1101@gmail.com>
2025-07-19 14:29:15 +0900

    font setting for linux

--- scripts/setting.sh
+++ scripts/setting.sh
@@ -7,11 +7,17 @@ if [ "$(uname)" = "Darwin" ]; then
   defaults write com.apple.dock appswitcher-all-displays -bool true
   defaults write com.apple.dock autohide -bool true
   killall Dock
+  mkdir -p /tmp/cica && cd /tmp/cica
+  curl -L -o Cica.zip https://github.com/miiton/Cica/releases/download/v5.0.3/Cica_v5.0.3.zip
+  unzip Cica.zip
+  cp -v ./*.ttf ~/Library/Fonts/
+  cd -
+else
+  mkdir -p /tmp/cica && cd /tmp/cica
+  curl -L -o Cica.zip https://github.com/miiton/Cica/releases/download/v5.0.3/Cica_v5.0.3.zip
+  unzip Cica.zip
+  mkdir -p ~/.local/share/fonts
+  cp -v ./*.ttf ~/.local/share/fonts/
+  fc-cache -fv
 fi
 
-# Font setting
-mkdir -p /tmp/cica && cd /tmp/cica
-curl -L -o Cica.zip https://github.com/miiton/Cica/releases/download/v5.0.3/Cica_v5.0.3.zip
-unzip Cica.zip
-cp -v ./*.ttf ~/Library/Fonts/
-cd -
