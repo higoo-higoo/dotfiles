76ac3dd91ed24c988f8f7b0df7ae0e6c2a401d13
higoo-higoo <higyuki1101@gmail.com>
2025-07-19 11:58:42 +0900

    add Brewfile

--- scripts/setting.sh
+++ scripts/setting.sh
@@ -11,4 +11,5 @@ killall Dock
 mkdir -p /tmp/cica && cd /tmp/cica
 curl -L -o Cica.zip https://github.com/miiton/Cica/releases/download/v5.0.3/Cica_v5.0.3.zip
 unzip Cica.zip
-cp -v *.ttf ~/Library/Fonts/
+cp -v ./*.ttf ~/Library/Fonts/
+cd -
