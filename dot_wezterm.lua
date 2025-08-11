28c83945ffe1085223a0ed5b54cc9c78da892a4b
higoo-higoo <higyuki1101@gmail.com>
2025-08-11 18:08:43 +0900

    update wezterm.lua zshrc

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -9,48 +9,48 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
-enable_kitty_graphics=true
+enable_kitty_graphics = true
 
 -- This is where you actually apply your config choices
 
 -- Window:
 config.color_scheme = "Dracula"
-config.window_background_opacity = 0.825
+config.window_background_opacity = 0.9
 config.use_fancy_tab_bar = false
 wezterm.on("gui-startup", function()
-	local tab, pane, window = mux.spawn_window({})
-	window:gui_window():maximize()
+  local tab, pane, window = mux.spawn_window({})
+  window:gui_window():maximize()
 end)
 
 -- Font:
 config.font = wezterm.font("Cica")
-config.font_size = 18
+config.font_size = 16
 config.use_ime = true
 
 -- KeyMap
 config.keys = {
-	{
-		key = "LeftArrow",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "RightArrow",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "UpArrow",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "DownArrow",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Down"),
-	},
+  {
+    key = "LeftArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Left"),
+  },
+  {
+    key = "RightArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Right"),
+  },
+  {
+    key = "UpArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Up"),
+  },
+  {
+    key = "DownArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Down"),
+  },
 }
 
 -- and finally, return the configuration to wezterm
