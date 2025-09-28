c552708e71cdb26493a062e346e583fca5f469aa
higoo-higoo <101345363+higoo-higoo@users.noreply.github.com>
2025-09-28 11:36:51 +0900

    udpate nvim config (#7)

--- dot_config/nvim/lua/plugins/codecompanion.lua
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -1,7 +1,18 @@
 require("codecompanion").setup({
-	opts = {
-		log_level = "DEBUG",
-	},
+  opts = {
+    log_level = "DEBUG",
+  },
+  strategies = {
+    chat = {
+      keymaps = {
+        send = {
+          modes = {
+            n = "<CR>",
+            i = "<C-l>"
+          }
+        }
+      }
+    }
+  }
 })
 vim.keymap.set("n", "<Leader>cc", "<cmd>CodeCompanionChat<CR>")
-
--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -3,7 +3,7 @@ vim.g.copilot_no_tab_map = true
 local keymap = vim.keymap.set
 -- https://github.com/orgs/community/discussions/29817#discussioncomment-4217615
 vim.g.copilot_no_tab_map = true
-vim.g.copilot_filetypes = {markdown = true, text = true, yaml = true}
+vim.g.copilot_filetypes = {['*'] = true}
 keymap("i", "<C-s>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true, replace_keycodes = false })
 keymap("i", "<C-j>", "<Plug>(copilot-next)")
 keymap("i", "<C-k>", "<Plug>(copilot-previous)")
