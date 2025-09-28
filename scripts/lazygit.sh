bef6fa170a16b31562f0a6361e6bb526f3cb0730
higoo-higoo <101345363+higoo-higoo@users.noreply.github.com>
2025-09-28 10:16:30 +0900

    update git config (#6)

--- scripts/lazygit.sh
+++ scripts/lazygit.sh
@@ -9,6 +9,7 @@ fi
 
 if [ "$(uname)" = "Darwin" ]; then
 	brew install lazygit
+	brew install git-delta
 elif [ "$(uname -m)" = "x86_64" ]; then
 	LAZYGIT_VERSION="$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')"
 	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
