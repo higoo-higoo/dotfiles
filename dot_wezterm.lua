824f5e7f6c56300ea443e238656b30e779269ca8
higoo-higoo <higyuki1101@gmail.com>
2025-11-08 22:48:52 +0900

    wezterm confをupdate

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -6,19 +6,25 @@ local mux = wezterm.mux
 -- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
+-- Set CTRL+V as the leader key (mod equivalent)
+config.leader = {
+  key = "g",
+  mods = "CTRL",
+  timeout_milliseconds = 1000,
+}
 enable_kitty_graphics = true
 
 -- This is where you actually apply your config choices
 
 -- Window:
 config.color_scheme = "Dracula"
-config.window_background_opacity = 0.9
+config.tab_bar_at_bottom = true
+config.window_background_opacity = 0.875
 config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
 wezterm.on("gui-startup", function()
   local tab, pane, window = mux.spawn_window({})
   window:gui_window():maximize()
@@ -26,32 +32,44 @@ end)
 
 -- Font:
 config.font = wezterm.font("PlemolJP Console NF")
-config.font_size = 13
+config.font_size = 14
 config.use_ime = true
 
 -- KeyMap
 config.keys = {
   {
     key = "LeftArrow",
-    mods = "CTRL|SHIFT",
+    mods = "LEADER",
     action = act.ActivatePaneDirection("Left"),
   },
   {
     key = "RightArrow",
-    mods = "CTRL|SHIFT",
+    mods = "LEADER",
     action = act.ActivatePaneDirection("Right"),
   },
   {
     key = "UpArrow",
-    mods = "CTRL|SHIFT",
+    mods = "LEADER",
     action = act.ActivatePaneDirection("Up"),
   },
   {
     key = "DownArrow",
-    mods = "CTRL|SHIFT",
+    mods = "LEADER",
     action = act.ActivatePaneDirection("Down"),
   },
+  {
+    key = "%",
+    mods = "LEADER",
+    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = '"',
+    mods = "LEADER",
+    action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
 }
+
+
 
 -- and finally, return the configuration to wezterm
 return config
