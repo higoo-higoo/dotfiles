c332a749d88b934e457f9a7e518a9df0a7b62bb7
higoo-higoo <higyuki1101@gmail.com>
2025-12-03 11:47:06 +0900

    lasygit_versionの取得方法を修正

--- scripts/lazygit.sh
+++ scripts/lazygit.sh
@@ -11,13 +11,13 @@ if [ "$(uname)" = "Darwin" ]; then
 	brew install lazygit
 	brew install git-delta
 elif [ "$(uname -m)" = "x86_64" ]; then
-	LAZYGIT_VERSION="$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')"
+  LAZYGIT_VERSION="$(curl -s https://api.github.com/repos/jesseduffield/lazygit/releases/latest | grep -m 1 'tag_name' | cut -d '"' -f4)"
 	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 	tar xf lazygit.tar.gz lazygit
 	sudo install lazygit /usr/local/bin
 	rm lazygit.tar.gz lazygit
 else
-	LAZYGIT_VERSION="$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')"
+  LAZYGIT_VERSION="$(curl -s https://api.github.com/repos/jesseduffield/lazygit/releases/latest | grep -m 1 'tag_name' | cut -d '"' -f4)"
 	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_arm64.tar.gz"
 	tar xf lazygit.tar.gz lazygit
 	sudo install lazygit /usr/local/bin
