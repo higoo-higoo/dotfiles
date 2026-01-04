4e0dfc309584cab8ac50efb900f4f4c3f16ef438
higoo-higoo <higyuki1101@gmail.com>
2026-01-04 15:38:29 +0900

    sketchybar

---
+++ dot_config/sketchybar/plugins/executable_calendar.sh
@@ -0,0 +1,7 @@
+events=$(gcalcli agenda --calendar Normal --tsv)
+
+# ヘッダーを除き、start_timeが空でない最初の予定を抽出
+next_event=$(echo "$events" | awk -F'\t' 'NR>1 && $2 != "" {print $1 " " $2 " - " $5; exit}')
+
+# sketchybarで表示（item_nameはsketchybarの設定に合わせて変更）
+sketchybar --set calendar_item label="$next_event"
---
+++ dot_config/sketchybar/plugins/executable_cpu.sh
@@ -0,0 +1,2 @@
+CPU=$(top -l 1 -n 0 | grep "CPU usage" | awk '{print $3}' | tr -d '%')
+sketchybar --set "$NAME" label="$CPU%"
